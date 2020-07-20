34482da981be39e8910fbe3413a19df972a5bc3a
Danzam97 <65907515+Danzam97@users.noreply.github.com>
2020-07-20 00:17:58 -0600

    Repositorio Tercer Parcial

---
+++ Tercer_Parcial/Tercer_Parcial/Menu.vb
@@ -0,0 +1,6 @@
+﻿Public Class fmMenu
+    Private Sub Trabajos_Click(sender As Object, e As EventArgs) Handles btnTrabajos.Click
+        fmTrabajos.Show()
+        Me.Hide()
+    End Sub
+End Class
---
+++ Tercer_Parcial/Tercer_Parcial/Trabajos.vb
@@ -0,0 +1,6 @@
+﻿Public Class fmTrabajos
+    Private Sub btnUsuarios_Click(sender As Object, e As EventArgs) Handles btnUsuarios.Click
+        fmUsuarios.Show()
+        Me.Hide()
+    End Sub
+End Class
